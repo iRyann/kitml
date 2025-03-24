Fix : Transposition in metric gradient

kitml/metrics/logLoss.py
@@ -12,6 +12,6 @@ class LogLoss(Metric) :
     
     def gradientsForNeuron(self, y_true, y_pred, x) :
 
-        dw = 1 / len(y_true) * np.dot(x.T, y_pred - y_true.T)
-        db = 1 / len(y_true) * np.sum(y_pred - y_true.T)
+        dw = 1 / len(y_true) * np.dot(x.T, y_pred - y_true)
+        db = 1 / len(y_true) * np.sum(y_pred - y_true)
         return (dw, db)